9aa01d9bcec11ce47e3757af1f116672ec08ed05
Jaseem <napsterjaseem@gmail.com>
2025-05-19 06:59:01 +0530

    Fix static files loading errors

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@
 const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/travels' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/travels/' : '',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 export default nextConfig
